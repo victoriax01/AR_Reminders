e5d26ea6300ec98ba8c31fff150434963f6878ba
victoriax01 <victoria.xiao.19@ucl.ac.uk>
2023-02-08 11:58:53 +0000

    added cup and some scripts

---
+++ Assets/DragScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+    private Touch touch;
+    private float speedModifier;
+
+    void Start()
+    {
+        speedModifier = 0.001f;
+    }
+
+
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved) {
+                transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speedModifier, 
+                transform.position.y, 
+                transform.position.z + touch.deltaPosition.y + speedModifier);
+            }
+        }
+    }
+}
